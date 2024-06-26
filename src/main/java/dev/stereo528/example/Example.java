e4277470c23f1c19288c19783ad7f278a14aaf44
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2024-06-26 11:54:18 -0600

    Initial Commit

---
+++ src/main/java/dev/stereo528/example/Example.java
@@ -0,0 +1,10 @@
+package dev.stereo528.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Example implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/stereo528/example/client/ExampleClient.java
@@ -0,0 +1,10 @@
+package dev.stereo528.example.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
